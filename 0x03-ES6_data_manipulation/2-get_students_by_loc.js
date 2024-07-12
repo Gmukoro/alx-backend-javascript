b94052250264037d8c2bb8d2e7ba840f3f8dc776
gmukoro <gmukoro3@gmail.com>
2024-07-12 10:29:42 +0100

    2-get_students_by_loc.js

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of studnts.
+ * @param {String} city - The location.
+ * @author Godspower Mukoro <https://github.com/Gmukoro>
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
