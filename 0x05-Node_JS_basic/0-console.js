d610a61259b5fe6bd925dc276b4e598df7fc6f48
gmukoro <gmukoro3@gmail.com>
2024-09-02 03:58:35 +0100

    Updated eslink

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Godspower Mukoro  <https://github.com/Gmukoro>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
